c32d51b35cd9b9092d3c1aa33c48ed06c58c54fe
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-11 13:21:26 +0100

    Updates

---
+++ precache-manifest.814282be12aa1631e9c817fe4ad5e2c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb34788983a501db77714fe09ed638f",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "89f1f5099e51eea2567c",
+    "url": "/React-Todo-List/static/css/main.46d09ef6.chunk.css"
+  },
+  {
+    "revision": "be38b871e95edd0da4f1",
+    "url": "/React-Todo-List/static/js/2.fd502326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-List/static/js/2.fd502326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f1f5099e51eea2567c",
+    "url": "/React-Todo-List/static/js/main.74643957.chunk.js"
+  },
+  {
+    "revision": "6eee05e4d053f5be312d",
+    "url": "/React-Todo-List/static/js/runtime-main.f20bc567.js"
+  },
+  {
+    "revision": "aea5529f9d1aaa7f05d6e7e952baa775",
+    "url": "/React-Todo-List/static/media/ishan-seefromthesky-6U-sSfBV-gM-unsplash.aea5529f.jpg"
+  }
+]);
